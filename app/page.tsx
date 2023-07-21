447739bf252e0a6d3afca2b8c0941cf2622b7615
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-07-21 21:08:35 +0200

    works with rainbowkit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,29 @@
 
 import { useState } from "react";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, zora],
+  [publicProvider()]
+);
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  projectId: "09c8084cdc92e99e928f07f537d62ad1",
+  chains,
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
 export default function Home() {
   const [data, setData] = useState("");
   const [transaction, setTransaction] = useState("");
@@ -23,20 +46,25 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-col items-center justify-between">
-      <input
-        placeholder="Create a post"
-        onChange={(e) => setData(e.target.value)}
-        className="text-black px-2 py-1"
-      />
-      <button onClick={upload} className="text-black bg-white mt-2 px-12">
-        Upload text
-      </button>
-      {transaction && (
-        <div className="text-black bg-white mt-2 px-12">
-          Transaction: {transaction}
-        </div>
-      )}
-    </main>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <main className="flex flex-col items-center justify-between">
+          <ConnectButton />
+          <input
+            placeholder="Create a post"
+            onChange={(e) => setData(e.target.value)}
+            className="text-black px-2 py-1"
+          />
+          <button onClick={upload} className="text-black bg-white mt-2 px-12">
+            Upload text
+          </button>
+          {transaction && (
+            <div className="text-black bg-white mt-2 px-12">
+              Transaction: {transaction}
+            </div>
+          )}
+        </main>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
