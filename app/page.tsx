16155941cb80cbab4f1e548c0d0e9c21dc96e752
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-07-21 21:29:32 +0200

    fixed styling

--- app/page.tsx
+++ app/page.tsx
@@ -48,16 +48,21 @@ export default function Home() {
   return (
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains}>
-        <main className="flex flex-col items-center justify-between">
+        <main className="flex flex-col items-center justify-between py-4">
           <ConnectButton />
-          <input
-            placeholder="Create a post"
-            onChange={(e) => setData(e.target.value)}
-            className="text-black px-2 py-1"
-          />
-          <button onClick={upload} className="text-black bg-white mt-2 px-12">
-            Upload text
-          </button>
+          <div className="py-4 px-10 flex flex-row w-full justify-center items-center">
+            <input
+              placeholder="Post a review"
+              onChange={(e) => setData(e.target.value)}
+              className="border-solid border-2 border-gray-600 text-black px-2 py-1 w-3/5"
+            />
+            <button
+              onClick={upload}
+              className="text-black bg-white mx-2 px-12 w-1/5 border-solid border-2 border-gray-300"
+            >
+              Upload text
+            </button>
+          </div>
           {transaction && (
             <div className="text-black bg-white mt-2 px-12">
               Transaction: {transaction}
